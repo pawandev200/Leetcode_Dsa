ba1a30e8ab72e1c292c276ee7e8c8522ce98b093
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-12-27 01:05:23 +0530

    Time: 0 ms (100%), Space: 34.4 MB (12.88%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst-Recursion-2.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ TreeNode* solve(TreeNode* root, int key){
+    if(!root) return NULL; 
+    if(key < root->val) root->left = solve(root->left, key);
+    else if(key > root->val) root->right = solve(root->right, key);
+    else {
+        // if 0/1/2 child of deleting node: 
+        if(!root->left && !root->right) return NULL;
+        if(!root->left || !root->right) return root->left ? root->left: root->right;
+        if(root->left && root->right){
+            //finding the rightmost ele and replacing it with the curr node(root)
+            TreeNode* temp = root->left; 
+            while(temp->right) temp = temp->right; 
+            root->val = temp->val;
+            root->left = solve(root->left, temp->val); // deleting the right most ele
+        } 
+
+    }
+    return root; 
+ }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        return solve(root, key);
+    }
+};
