435f5db12a252f87bbceb2427957fa9b41500e0c
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-05 13:01:06 +0530

    Time: 42 ms (41.33%), Space: 108.2 MB (27.49%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
-        vector<int>netchanges(n+1, 0); // for precomputation, net change of each char
+        vector<int>changes(n+1, 0); // for precomputation, net change of each char
 
         for(auto it: shifts){
             int c1 = it[0];
             int c2 = it[1];
             int dir = it[2];
 
-            netchanges[c1] += (dir == 1 ? 1 : -1);
-            netchanges[c2 + 1] -= (dir == 1 ? 1 : -1);
+            changes[c1] += (dir == 1 ? 1 : -1);
+            changes[c2 + 1] -= (dir == 1 ? 1 : -1);
         }
 
-        int shift = 0; // Tracks the net shift at each position
+        // finding the prefix sum: for net changes at each index
+        int shift = 0;
         for (int i = 0; i < n; i++) {
-           shift += netchanges[i]; // Accumulate the net shift
-           shift = (shift % 26 + 26) % 26; 
+           shift += changes[i]; 
+           shift = (shift % 26 + 26) % 26; // wrapping the shift around a<->z
            s[i] = 'a' + (s[i] - 'a' + shift) % 26; 
         }
         return s; 
