a1d4befde6caee34b935ee6729d0b43d36895097
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-10 12:00:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (72.62%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        //mapping: direct and Subtractive form(4... 9...).
-        vector<pair<int, string>> mp = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
-
-        string result = "";
-        // Iterating through the mapping and subtracting values while appending symbols.
-        for (auto &[val, symb] : mp) {
-            while (num >= val) {
-                result += symb;
-                num -= val;
-            }
-        }
-
-        return result;
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
