d41dc3269aced419074c24c82232528b98189dcf
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-10 11:57:51 +0530

    Time: 5 ms (41.25%), Space: 12.6 MB (46.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        //mapping: direct and Subtractive form(4... 9...)
         vector<pair<int, string>> mp = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
@@ -9,10 +10,10 @@ public:
 
         string result = "";
 
-        for (auto &[value, symbol] : mp) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
+        for (auto &[val, symb] : mp) {
+            while (num >= val) {
+                result += symb;
+                num -= val;
             }
         }
 
