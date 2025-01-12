217c5fb1d5e1eb295bb11a4b5406bb236b67f8e9
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-12 12:07:40 +0530

    Time: 12 ms (68.27%), Space: 30.3 MB (34.26%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -4,15 +4,16 @@ public:
         int n = s.size();
         if(n%2 !=0) return false;  // If the length is odd, it can't be balanced
 
-        int opencnt = 0; 
         // Left-to-right pass: Check if we can open enough to balance so far
+        int opencnt = 0; 
         for(int i=0; i<n; i++){
             if(s[i] == '(' || locked[i] == '0') opencnt++; 
             else opencnt--; 
             if(opencnt < 0) return false; // Too many closing brackets
         }
-        int closecnt = 0;
+        
         // Right-to-left pass: Check if we can close enough to balance so far 
+        int closecnt = 0;
         for(int i= n-1; i>=0; i--){
             if(s[i] == ')' || locked[i] == '0') closecnt++; 
             else closecnt--; 
