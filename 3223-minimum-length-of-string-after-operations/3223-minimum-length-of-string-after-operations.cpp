ee33ebf9bbd080d20c10e003e817d9c13874cdee
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-13 10:42:18 +0530

    Time: 13 ms (71.77%), Space: 32.1 MB (74.19%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -3,9 +3,12 @@ public:
     int minimumLength(string s) {
         int n = s.size();
         int minus = 0;
+        
+        // Count the frequency of each character
         vector<int>freq(26);
         for(int i =0; i<n; i++) freq[s[i] - 'a']++; 
 
+        // For each character, remove pairs (if >= 3)
         for(int i=0; i<26; i++){
             while(freq[i] >=3){
                 minus+=2; 
