8d55ffabb15615875b24166473a69620af5d6971
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-14 10:44:44 +0530

    Time: 1 ms (21.62%), Space: 19.7 MB (56.77%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -11,40 +11,32 @@
  */
 class Solution {
 public:
-int solve(TreeNode* root) {
-    if (!root) return 0; 
-
-    queue<pair<TreeNode*, long long>> q; // nodes, index
-    q.push({root, 0});
-    int maxw = 1; 
-
-    while (!q.empty()) {
-        int size = q.size();
-        int f = 0, l = 0; 
-        long long midx = q.front().second; // 1st node of curr lev (for normalization)
-
-        for (int i = 0; i < size; i++) {
-            TreeNode* node = q.front().first;
-            long long idx = q.front().second - midx; // Normalize index
-            q.pop();
-
-            if (i == 0) f = idx;  // First node of the level
-            if (i == size - 1) l = idx; // Last node of the level
-
-            // Push children with their respective indices
-            if (node->left) q.push({node->left, 2 * idx + 1});
-            if (node->right) q.push({node->right, 2 * idx + 2});
-        }
-
-        // Update the maximum width
-        maxw = max(maxw, l - f + 1);
-    }
-
-    return maxw; 
-}
-
     int widthOfBinaryTree(TreeNode* root) {
-        
-        return solve(root);
+        if(!root) return 0; 
+        queue<pair<TreeNode*, long long>>q; // node, idx; 
+        q.push({root, 0});
+        long long w =1; 
+        long long maxw = 1; 
+        while(!q.empty()){
+            int size = q.size();
+            int li = 0, ri = 0;
+            long long norm = q.front().second; // for normalization
+            for(int i=0; i<size; i++){
+                TreeNode* node = q.front().first;
+                long long idx = q.front().second - norm;
+                q.pop();
+                // tracking width after each level:  
+                if(i ==0) li = idx;
+                if(i == size -1){
+                    ri = idx;
+                    w = ri - li + 1;
+                }
+
+                if(node->left) q.push({node->left, 2*idx+1});
+                if(node->right) q.push({node->right, 2*idx+2});
+            }
+            maxw = max(maxw, w);
+        }
+        return maxw; 
     }
 };
