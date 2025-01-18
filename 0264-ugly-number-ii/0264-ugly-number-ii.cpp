c4f573599aa343dd2cc566d8fec20c4789bdc23c
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-18 23:17:01 +0530

    Time: 79 ms (30.34%), Space: 33.4 MB (17.23%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         priority_queue<long, vector<long>, greater<long>> minHeap;
-        unordered_set<long> seen;
+        unordered_set<long> st;
         
         minHeap.push(1);  // Start with 1 as the first ugly number
-        seen.insert(1);
+        st.insert(1);
         
         long uglyNumber = 0;
         
@@ -13,18 +13,18 @@ public:
             uglyNumber = minHeap.top();  // Get the smallest number from the heap
             minHeap.pop();
             
-            // next possible ugly numbers and push them into the heap if not seen
-            if (seen.find(uglyNumber * 2) == seen.end()) {
+            // next possible ugly numbers and push them into the heap if not st
+            if (st.find(uglyNumber * 2) == st.end()) {
                 minHeap.push(uglyNumber * 2);
-                seen.insert(uglyNumber * 2);
+                st.insert(uglyNumber * 2);
             }
-            if (seen.find(uglyNumber * 3) == seen.end()) {
+            if (st.find(uglyNumber * 3) == st.end()) {
                 minHeap.push(uglyNumber * 3);
-                seen.insert(uglyNumber * 3);
+                st.insert(uglyNumber * 3);
             }
-            if (seen.find(uglyNumber * 5) == seen.end()) {
+            if (st.find(uglyNumber * 5) == st.end()) {
                 minHeap.push(uglyNumber * 5);
-                seen.insert(uglyNumber * 5);
+                st.insert(uglyNumber * 5);
             }
         }
         
