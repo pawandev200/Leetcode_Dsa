64dc22395dc6545b06dd875c7ed7eda850b68a0e
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-18 22:48:00 +0530

    Time: 3 ms (65.17%), Space: 18.2 MB (31.95%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -7,11 +7,11 @@ public:
 
         //sorted based on freq: top k frequent need to keep: min - heap: 
         // Min-Heap with custom comparator to sort by frequency, and lexicographically for ties.
-        auto cmp = [](pair<int, string>& a, pair<int, string>& b) {
-            if (a.first == b.first) return a.second < b.second; // Lexicographical order for ties.
-            return a.first > b.first; // Min-Heap based on frequency.
+        auto comp = [](pair<int, string>&a, pair<int, string>&b){
+            if(a.first == b.first) return a.second < b.second; 
+            return a.first > b.first; 
         };
-        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> pq(cmp); // freq, string
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(comp)>pq(comp); // freq, string
 
         for(auto it: mp){
             pq.push({it.second, it.first});
@@ -22,7 +22,7 @@ public:
             ans.push_back(pq.top().second);
             pq.pop();
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(), ans.end()); // since least freq on top of pq
         return ans; 
     }
 };
