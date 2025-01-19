cfb8dfedb55046ceaa0675e18affc02911431fdf
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-20 01:58:32 +0530

    Time: 13 ms (21.3%), Space: 17.2 MB (60.69%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
-        priority_queue<int>maxheap;
-        for(int i =0; i<n; i++){
-            for(int j =0; j<n; j++){
-                maxheap.push(matrix[i][j]);
 
-                if(maxheap.size() > k) maxheap.pop();
+        // Min-heap to store values and their coordinates
+        using piii = pair<int, pair<int, int>>;
+        priority_queue<piii, vector<piii>, greater<piii>> minheap;
+
+        // Push the first element of each row into the heap
+        for (int i = 0; i < n; i++) {
+            minheap.push({matrix[i][0], {i, 0}});
+        }
+
+        // Extract the smallest element from the heap k times
+        while (!minheap.empty()) {
+            auto [val, indices] = minheap.top();
+            minheap.pop();
+            k--;
+            if (k == 0) return val; // Return the kth smallest element
+
+            int row = indices.first;
+            int col = indices.second;
+
+            // Push the next element in the same row into the heap
+            if (col + 1 < n) {
+                minheap.push({matrix[row][col + 1], {row, col + 1}});
             }
         }
-        return maxheap.top();  
+
+        return -1; // This line should not be reached
     }
 };
