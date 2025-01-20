338db555ddf913e5f4f351bf56ffe02370111d64
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-01-20 11:33:29 +0530

    Time: 27 ms (14.95%), Space: 19.8 MB (12.18%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convert(string s, int r) {
+        int n = s.size();
+        if(r==1 || r>=n) return s; 
+
+        vector<vector<char>>rows(r);
+        int idx = 0; 
+        int d = 1; 
+        for(int i =0; i<n; i++){
+            rows[idx].push_back(s[i]);
+            if(idx == 0) d =1; 
+            if(idx == r-1) d= -1; 
+            idx +=d; 
+        }
+
+        string ans; 
+        for(auto it: rows){
+            for(int ch: it){
+                ans+=ch;
+            }
+        }
+        return ans; 
+    }
+};
