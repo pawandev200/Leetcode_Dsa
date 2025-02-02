1ecbe839fd19bc15e754de5d3b88e8b083aa95ce
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-02 22:56:32 +0530

    Time: 61 ms (14.88%), Space: 159 MB (8.41%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+vector<vector<int>>dp; 
+int solve(int i, int lc, int n){
+    // base case:
+    if(i == n) return 0;
+    if(i>n) return n;  
+
+    if(dp[i][lc] !=-1) return dp[i][lc]; 
+
+    int paste = 1 + solve(i+lc, lc, n);
+    int copy = INT_MAX;
+    if(i!=lc) copy = 1 + solve(i, i, n);
+
+    return dp[i][lc] = min(copy, paste);
+}
     int minSteps(int n) {
-        int steps = 0;
-        for (int i = 2; i * i <= n; i++) {
-            while (n % i == 0) {
-                steps += i;
-                n /= i;
-            }
-        }
-        if (n > 1) {
-            steps += n;
-        }
-        return steps;
+        if(n==1) return 0;
+        dp.resize(n+1, vector<int>(n+1, -1));
+
+        return 1 + solve(1, 1, n); 
     }
 };
