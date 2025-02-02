df5d11687e72c7d8e9b9e5d51052e6f7d72d84d3
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-02 22:52:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.24%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int solve(int i, int t) {
-        // base cases
-        if (t == 1) return 0; // no more steps needed
-        if (dp[i][t] != -1) return dp[i][t];
-        
-        int res = INT_MAX;
-        for (int j = 1; j < t; j++) {
-            if (t % j == 0) {
-                res = min(res, solve(i, j) + (t / j));
+    int minSteps(int n) {
+        int steps = 0;
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                steps += i;
+                n /= i;
             }
         }
-        
-        return dp[i][t] = res;
-    }
-    
-    int minSteps(int n) {
-        if (n == 1) return 0;
-        dp.resize(n + 1, vector<int>(n + 1, -1));
-        return solve(1, n);
+        if (n > 1) {
+            steps += n;
+        }
+        return steps;
     }
 };
