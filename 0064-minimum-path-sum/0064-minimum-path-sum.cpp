9a17e3bfbbf6d59bfc35f90724c66e221b9bcfec
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-07 01:56:52 +0530

    Time: 3 ms (31.34%), Space: 16.3 MB (38.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
 
- int solve( int i, int j,vector< vector<int>> &dp, vector<vector<int>>& grid){
-    //base case
+ vector<vector<int>>dp;
+ int solve( int i, int j, vector<vector<int>>& grid){
+    // base case
+    if(i<0 || j<0) return 1e9;
     if(i==0 && j==0) return grid[0][0];
-    else if( i<0 || j<0) return 1e9;
 
     // memoization check
     if(dp[i][j]!=-1) return dp[i][j];
 
     // working of current indices
-    int up= grid[i][j] + solve(i-1,j, dp, grid);
-    int left= grid[i][j] + solve(i,j-1, dp, grid);
+    int up= grid[i][j] + solve(i-1,j,grid);
+    int left= grid[i][j] + solve(i,j-1,grid);
 
     // storing and returning 
     return dp[i][j] = min(up, left);
@@ -20,11 +21,10 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size(); // numer of raw
         int n=grid[0].size();// number of coloum 
-        vector< vector<int>> dp(m, vector<int>(n,-1));
-        int ans= solve(m-1, n-1, dp, grid);
+        dp.resize(m, vector<int>(n,-1));
+        int ans= solve(m-1, n-1, grid);
 
         if(ans>1e9) return 0; 
-         return ans; 
-        
+        return ans;  
     }
 };
