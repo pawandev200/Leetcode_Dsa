5e5d39e9bb56bfbdafef3763edf4f4803ec1b474
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-07 10:58:47 +0530

    Time: 3 ms (19.42%), Space: 8.4 MB (39.33%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getNextNumber(int n) {
+        int output = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+        return output;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> visit;
+        while (visit.find(n) == visit.end()) {
+            visit.insert(n);
+            n = getNextNumber(n);
+            if (n == 1) return true;
+        } 
+        return false;
+    }
+};
