177e2034c756be2df5d8ca487b5e6546988cb34f
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-09 01:57:24 +0530

    Time: 0 ms (100%), Space: 17.2 MB (58.57%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+
+// just when you get the profit sell it, count the profit
+//  add the profit for the later transections 
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int profit = 0; 
-        for(int i=0; i<n-1; i++){
-            if(prices[i] < prices[i+1]){
-                profit+=prices[i+1] - prices[i];
+        int profit = 0; // minimum profit 
+        for(int i=1; i<n; i++){
+            // buy on ith day and sell on (i+1)th day if there is profit  
+            if(prices[i-1] < prices[i]){ 
+                profit+=prices[i] - prices[i-1];
             }
         }
         return profit; 
