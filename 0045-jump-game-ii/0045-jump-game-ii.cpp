6a99788c06043d151006a85b345532670965980b
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-08 22:31:34 +0530

    Time: 208 ms (8.28%), Space: 22.6 MB (6.74%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
+    vector<int>dp; 
+    int solve(int i, vector<int>& nums){
+        // base case:
+        if(i>=nums.size()-1) return 0;
 
-int solve(int i, vector<int>& nums,vector<int>&dp){
-    if(i>= nums.size()-1) return 0;
-    
-    if(dp[i]!=-1) return dp[i];
-    int mini = 1e9;
-    for(int j=1; j<=nums[i]; j++){
-        int steps = 1 + solve(i+j, nums, dp);
-        mini = min(mini, steps);
+        if(dp[i]!=-1) return dp[i];
+        int ans = 1e9; 
+        for(int c=1; c<=nums[i]; c++){
+            int step = 1 + solve(i+c, nums);
+            ans = min(ans, step);
+        }
+        return dp[i] = ans; 
     }
-
-  return dp[i] = mini;
-}
     int jump(vector<int>& nums) {
         int n = nums.size();
-        if(n ==0) return 0;
-        if(nums[0] ==0) return 0;
-        vector<int>dp(n+1, -1);
-        return solve(0, nums, dp);
+        dp.resize(n,-1);
+        return solve(0, nums);
     }
 };
