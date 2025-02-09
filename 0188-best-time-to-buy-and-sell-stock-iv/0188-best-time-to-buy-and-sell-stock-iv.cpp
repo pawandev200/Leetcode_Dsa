9fc70c5be26d31e28501faf3a8b8ac7a0157b038
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-09 23:02:17 +0530

    Time: 0 ms (100%), Space: 14.2 MB (98.01%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        if (n == 0 || k == 0) return 0;
+        if(n ==0 || k==0) return 0; 
 
-        // Initialize buy and profit arrays
-        vector<int> buy(k, INT_MAX); // Eff buying price for each transaction
-        vector<int> profit(k, 0);    // Maximum profit after each transaction
+        vector<int>buy(k, INT_MAX);
+        vector<int>profit(k, 0);
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             int cprice = prices[i];
-
-            // Update buy and profit for each transaction
-            for (int j = 0; j < k; j++) {
-                // Effective buying price for the j-th transaction
-                buy[j] = min(buy[j], cprice - (j > 0 ? profit[j - 1] : 0));
-
-                // Maximum profit after the j-th transaction
+            for(int j = 0; j<k; j++){
+                buy[j] = min(buy[j], cprice - (j > 0 ? profit[j-1] : 0));
                 profit[j] = max(profit[j], cprice - buy[j]);
             }
         }
-        return profit[k - 1]; // max profit after k transections
+        return profit[k-1];
     }
 };
