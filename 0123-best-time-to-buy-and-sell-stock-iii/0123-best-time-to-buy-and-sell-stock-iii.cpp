182ce0c2d53e82a106305faa75e2c7292706585e
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-10 03:10:51 +0530

    Time: 4 ms (85.28%), Space: 79.2 MB (88.64%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+// After making profit p1 from the 1st transaction, for the 2nd transaction,
+// suppose buying amount is x then net you are spending is x - p1 and then sell 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int b1 = INT_MAX; 
@@ -8,10 +10,11 @@ public:
         int p2 = 0; 
         for(int i=0; i<n; i++){
             int cval = prices[i];
-            b1 = min(b1, cval);
+            b1 = min(b1, cval); //lowest price seen so far for 1st transection
             p1 = max(p1, cval - b1); // getting max val from sell-1
             
-            b2 = min(b2, cval-p1);
+            // effective buying price for the 2nd transection
+            b2 = min(b2, cval-p1); //lowest price seen so far for 2nd transection
             p2 = max(p2, cval - b2); // getting max val from sell-2
         }
         return p2; 
