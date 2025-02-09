06f5390f39f4eeccc5433bd0e5fc1c052d8d8aed
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-09 22:26:45 +0530

    Time: 4 ms (85.39%), Space: 79.2 MB (95.64%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,16 +4,16 @@ public:
         int n = prices.size();
         int b1 = INT_MAX; 
         int b2 = INT_MAX; 
-        int s1 = 0; 
-        int s2 = 0; 
+        int p1 = 0; 
+        int p2 = 0; 
         for(int i=0; i<n; i++){
             int cval = prices[i];
             b1 = min(b1, cval);
-            s1 = max(s1, cval - b1); // getting max val from sell-1
+            p1 = max(p1, cval - b1); // getting max val from sell-1
             
-            b2 = min(b2, cval-s1);
-            s2 = max(s2, cval - b2); // getting max val from sell-2
+            b2 = min(b2, cval-p1);
+            p2 = max(p2, cval - b2); // getting max val from sell-2
         }
-        return s2; 
+        return p2; 
     }
 };
