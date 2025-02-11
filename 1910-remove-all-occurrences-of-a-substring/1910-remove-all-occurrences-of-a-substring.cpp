80a5dcee0f865b60169ac64ce40bdb088b5b220e
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-11 10:41:57 +0530

    Time: 4 ms (15.12%), Space: 12.1 MB (9.01%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string ans = "";
+        int n = part.size();
+        
+        for (char c : s) {
+            ans += c;
+            if (ans.size() >= n && ans.substr(ans.size() - n) == part) {
+                ans.erase(ans.size() - n);
+            }
+        }
+        return ans;
+    }
+};
