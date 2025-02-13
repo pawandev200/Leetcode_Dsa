5f0ae514ae43cacae0eb9a72b97e5c8171eef361
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-14 01:50:46 +0530

    Time: 42 ms (13.93%), Space: 14.1 MB (21.91%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -17,7 +17,7 @@ public:
     }
 
     int minCost(int n, vector<int>& cuts) {
-        // Adding 0 and n at Boundaries: 
+        // Adding 0 and n at Boundaries: to cover full range and to find 1st cut val
         // If n = 7 and cuts = [1, 3, 5], adding boundaries gives [0, 1, 3, 5, 7].
         // Now, we can define segments like (0, 1), (1, 3), (3, 5), and (5, 7).
         int c = cuts.size();
@@ -27,6 +27,6 @@ public:
         sort(cuts.begin(), cuts.end()); // to process from smallest to largest
 
         dp.resize(c + 2, vector<int>(c + 2, -1)); 
-        return solve(0, c + 1, cuts);
+        return solve(0, c + 1, cuts); // l = 0, r = cuts.size()-1;
     }
 };
