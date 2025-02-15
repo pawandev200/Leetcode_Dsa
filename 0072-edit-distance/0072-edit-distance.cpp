4a60510e3244c16c974eacd4b0fe1a6fecf68745
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-15 20:57:46 +0530

    Time: 5 ms (71.61%), Space: 14.4 MB (17.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-int solve(int i, int j, const string &s, const string &t, vector<vector<int>> &dp) {
+vector<vector<int>>dp;
+int solve(int i, int j, string &s, string &t) {
     // base cases
-    if (i < 0) return j + 1;
-    if (j < 0) return i + 1;
+    if (i >= s.size()) return t.size() - j; //reamining char of t need to be inserted
+    if (j >= t.size()) return s.size() - i; //remaining char of s need to be deleted
 
     // memoization check
     if (dp[i][j] != -1) return dp[i][j];
 
     // if characters match, no operation is needed
-    if (s[i] == t[j]) {
-        return dp[i][j] = solve(i - 1, j - 1, s, t, dp);
-    } else {
+    int ans =0; 
+    if (s[i] == t[j]) ans = solve(i + 1, j + 1, s, t);
+    else {
         // if characters do not match, consider all three operations
-        return dp[i][j] = 1 + min({solve(i - 1, j, s, t, dp),       // delete
-                                   solve(i, j - 1, s, t, dp),       // insert
-                                   solve(i - 1, j - 1, s, t, dp)}); // replace
+        int del = solve(i + 1, j, s, t); // delete
+        int ins =  solve(i, j + 1, s, t); // insert
+        int rep = solve(i + 1, j + 1, s, t); // replace
+        ans = 1 + min({del, ins, rep}); 
     }
+    return dp[i][j] = ans; 
 }
     int minDistance(string word1, string word2) {
 
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n-1, m-1, word1, word2, dp);
+        if(n ==0) return m; // all char of word2 need to inserted in word1
+        dp.resize(n, vector<int>(m, -1));
+        return solve(0, 0, word1, word2);
     }
 };
