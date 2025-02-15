13d2617209798d810935ae084e69b07c68bf218a
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-15 10:47:45 +0530

    Time: 91 ms (67.51%), Space: 10.1 MB (65.24%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool solve(int i, int t, string s){
+        if(i==s.size()) return t==0; 
+
+        int sum =0; 
+        for(int c = i; c<s.size(); c++){ // trying all possible partition: 
+            sum = sum*10 + (s[c]-'0');
+            if(sum > t) break; 
+            if(solve(c+1, t-sum, s)) return true; 
+        }
+        return false; 
+    }
+
+    int punishmentNumber(int n) {
+        // given conditions: 
+        //1. punishment number: i*i : 1<=i<=n
+        //2. partition of x(=i*i): recursive function for it. 
+
+        if(n ==0) return 0;
+        int ans = 0;        
+        for(int i=1; i<=n; i++){
+            int x = i*i;
+            string s = to_string(x); 
+            if(solve(0, i, s)==true) ans+=x; 
+        }
+        return ans; 
+    }
+};
