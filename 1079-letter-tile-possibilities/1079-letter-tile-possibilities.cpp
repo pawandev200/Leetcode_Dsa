80789e2d0b4306584dfa175aea4b78486a2b8955
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-21 22:46:49 +0530

    Time: 4 ms (50.74%), Space: 7.8 MB (87.39%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    // Start with every unique char, since it is used so decrease its freq by 1
+    // and count it for used char, call recursively with remaining freq 
+    // lastly backtrack the freq to try other options
+    
     int solve(vector<int>&freq){
         int ans = 0; 
-        for (int i = 0; i < 26; ++i){
+        for (int i = 0; i < 26; i++){
             if (freq[i] > 0) {
-                freq[i]--; 
-                ans+=1+solve(freq);
-                freq[i]++;
+                freq[i]--;  // currently used char 
+                ans += 1 + solve(freq); // for remaining char 
+                freq[i]++; // backtrack
             }
         }
         return ans; 
