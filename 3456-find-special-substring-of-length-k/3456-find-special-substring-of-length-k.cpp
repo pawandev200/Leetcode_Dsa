8f140ee785136e7b52bb067292f3eb0bef373db7
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-22 23:49:20 +0530

    Time: 0 ms (100%), Space: 8.4 MB (88.71%) - LeetHub

--- 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.cpp
+++ 3456-find-special-substring-of-length-k/3456-find-special-substring-of-length-k.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-    bool hasSpecialSubstring(string s, int k) {
-        int n = s.size();
-        if (k > n) return false; 
-
-        unordered_map<char, int> mp;
-        int i = 0, j = 0;
-
-        while (j < n) {
-            mp[s[j]]++; // Expand the window 
-            
-            // If window size reaches k
-            if (j - i + 1 == k) {
-                if (mp.size() == 1) { 
-                    bool leftOk = (i == 0 || s[i - 1] != s[i]);  
-                    bool rightOk = (j == n - 1 || s[j + 1] != s[j]); 
-                    if (leftOk && rightOk) return true;
-                }
-
-                // Shrink the window from the left
-                mp[s[i]]--;
-                if (mp[s[i]] == 0) mp.erase(s[i]);
-                i++;
+    bool hasSpecialSubstring(string& s, int k){
+        int n=s.size();
+        int f=1;
+        for(int i=1; i<n; i++){
+            if (s[i]!=s[i-1]){
+                if (f==k) return 1;
+                f=1;
             }
-            j++; // Expand the right side
+            else f++;
         }
-        return false;
+        return f==k;
     }
 };
