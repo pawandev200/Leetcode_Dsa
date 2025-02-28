699ead0d43a087dcaf8a153a14b9ddf8e131cae1
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-02-28 22:48:30 +0530

    Time: 2 ms (67.72%), Space: 11.8 MB (76.13%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string rn, string mag) {
+        int n = rn.size();
+        int m = mag.size();
+
+        vector<int>vect1(26); 
+        vector<int>vect2(26); 
+        for(auto ch: rn) vect1[ch-'a']++;
+        for(auto ch: mag) vect2[ch-'a']++;
+
+        for(int i=0; i<26; i++){
+            if(vect1[i]!=0 && vect2[i]==0) return false; 
+            if(vect1[i]!=0 && vect1[i] > vect2[i]) return false; 
+        }
+        return true; 
+    }
+};
