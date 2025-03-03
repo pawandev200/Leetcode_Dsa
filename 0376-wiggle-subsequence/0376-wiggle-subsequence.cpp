23c9126a12eaf01a4b59ce07af8ac2b1c16437e0
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-03 13:10:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -9,19 +9,18 @@ public:
         if(dp[i][l+1][s]!=-1) return dp[i][l+1][s];
 
         int diff = 0, take = 0; 
-        if(l!=-1) diff = (nums[i] - nums[l]);
+        if(l!=-1) diff = (nums[i] - nums[l]) > 0 ? 1 : 0;
 
-        if(l==-1 || (diff>0 && !s) || (diff<0 && s)){
-            take = 1 + solve(i+1, i, !s, nums);
-        }
+        if(l==-1 || diff != s) take = 1 + solve(i+1, i, !s, nums);
         int notTake = solve(i+1, l, s, nums);
 
         return dp[i][l+1][s] = max(take, notTake);
     }
     int wiggleMaxLength(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));
-        int f = solve(0, -1, true, nums);
-        int s = solve(0, -1, false, nums);
+        // equal number should ignored
+        int f = solve(0, -1, true, nums); // taking first ele
+        int s = solve(0, -1, false, nums); // notTaking the first ele
         return max(f, s);
     }
 };
