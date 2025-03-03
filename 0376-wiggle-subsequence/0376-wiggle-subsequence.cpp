66abd58f77629e670bc9ccf233899e601098c5e1
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-03 13:03:35 +0530

    Time: 25 ms (8.89%), Space: 18.9 MB (9.93%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    int dp[1001][1001][2];
+
     int solve(int i, int l, bool s, vector<int>&nums){
         //base case:
         if(i==nums.size()) return 0; 
 
-        int diff = (nums[i] - l) >0 ? 1: 0;
-        int take = 0;  
-        if(l==-1 || diff != s) take = 1 + solve(i+1, nums[i], !s, nums);
+        if(dp[i][l+1][s]!=-1) return dp[i][l+1][s];
+
+        int diff = 0, take = 0; 
+        if(l!=-1) diff = (nums[i] - nums[l]);
+
+        if(l==-1 || (diff>0 && !s) || (diff<0 && s)){
+            take = 1 + solve(i+1, i, !s, nums);
+        }
         int notTake = solve(i+1, l, s, nums);
 
-        return max(take, notTake);
+        return dp[i][l+1][s] = max(take, notTake);
     }
     int wiggleMaxLength(vector<int>& nums) {
-        return solve(0, -1, true, nums);
+        memset(dp, -1, sizeof(dp));
+        int f = solve(0, -1, true, nums);
+        int s = solve(0, -1, false, nums);
+        return max(f, s);
     }
 };
