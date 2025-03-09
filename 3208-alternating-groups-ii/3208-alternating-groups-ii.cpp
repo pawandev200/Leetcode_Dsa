341fb4b123d692f73280b91216fe772b7e819a6e
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-09 21:42:27 +0530

    Time: 63 ms (86.61%), Space: 102.5 MB (91.84%) - LeetHub

---
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        int count = 0;
+        int l = 0;
+        
+        for (int r = 0; r < n + k - 1; ++r) {
+            if (r > 0 && colors[r % n] == colors[(r - 1) % n]) l = r; 
+            if (r - l + 1 >= k) count++; 
+        }
+        return count;
+    }
+};
