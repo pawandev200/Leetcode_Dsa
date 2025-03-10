8c82fcda4842e4cd85639daac86f3af986ff38c7
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-11 01:19:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        int ans = -1; 
-        for(int i=1; i<n-1; i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i; 
+        int n = nums.size(); 
+        for(int i=0; i<n-1; i++){
+            // peak value is always greater than the left value
+            // just check the right value
+            if(nums[i] > nums[i+1]) return i;
         }
         return n-1; 
     }
