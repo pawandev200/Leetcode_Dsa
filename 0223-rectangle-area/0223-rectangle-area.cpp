278e01209aabec79fe144a14fcb94382ddc65a06
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-14 16:36:38 +0530

    Time: 0 ms (100%), Space: 10.6 MB (38.65%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = abs(ax2 - ax1) * abs(ay1 - ay2);
+        int area2 = abs(bx2 - bx1) * abs(by1 - by2);
+
+        // common area: 
+        int l = max(ax1, bx1);
+        int b = max(ay1, by1);
+        int r = min(ax2, bx2);
+        int t = min(ay2, by2);
+
+        int carea = 0; 
+        if(l<r && t>b) carea = (r-l)*(t-b);
+
+        return area1 + area2 - carea;   
+    }
+};
