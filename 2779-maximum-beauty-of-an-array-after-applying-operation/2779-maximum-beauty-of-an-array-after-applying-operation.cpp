c07fb1f87aa6b0aebdb9bf8ab2da9641592413b2
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-14 12:09:10 +0530

    Time: 531 ms (9.01%), Space: 191.8 MB (14.18%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        int maxi = 0; 
-        for(int num: nums) maxi = max(maxi, num);
-        vector<int>diff(maxi+2, 0);
+        // int maxi = 0; 
+        // for(int num: nums) maxi = max(maxi, num);
+        // vector<int>diff(maxi+2, 0);
+
+        map<int, int>diff; 
+
         for(int i=0; i<nums.size(); i++){
-            int s = (nums[i] - k) < 0 ? 0: nums[i] - k; 
-            int l = (nums[i] + k) > maxi ? maxi: nums[i] + k; 
-            
+            // int s = (nums[i] - k) < 0 ? 0: nums[i] - k; 
+            // int l = (nums[i] + k) > maxi ? maxi: nums[i] + k; 
+            int s = nums[i] - k; 
+            int l = nums[i] + k; 
             diff[s]++; 
             diff[l+1]--; 
         }
         int sum = 0;
         int ans = 0;  
-        for(int i=0; i<diff.size(); i++){
-            sum+=diff[i];
+        for(auto it: diff){
+            sum+=it.second;
             ans = max(ans, sum);
         }
         return ans; 
