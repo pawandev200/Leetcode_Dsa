403aa324c08a4086ad65b55864649ed6833f70b9
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-23 20:49:49 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.83%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,9 +3,7 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         for(int i = 0; i < n - 1; i += 2) {
-            if(nums[i] != nums[i + 1]) {
-                return nums[i]; // Found the unique element
-            }
+            if(nums[i] != nums[i + 1]) return nums[i]; 
         }
         return nums[n - 1]; // Unique element is at the last position
     }
