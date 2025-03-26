8cc9be8fd67a771e3c694dad5e9fe73465b79dd1
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-03-26 12:01:04 +0530

    Time: 37 ms (71.12%), Space: 90.1 MB (34.95%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int>nums; 
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                nums.push_back(grid[i][j]);
+            }
+        }
+        //checking making conversion is possible or not: 
+        int mod = nums[0]%x; 
+        for(auto num: nums){
+            if(num%x != mod) return -1; 
+        }
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        int med = 0; 
+        if(size%2==1) med = nums[size/2];
+        else med = nums[(size/2 + (size/2+1))/2];
+        
+        int cnt = 0; 
+        for(auto num: nums){
+            cnt+= abs(num-med)/x; 
+        }
+        return cnt; 
+    }
+};
