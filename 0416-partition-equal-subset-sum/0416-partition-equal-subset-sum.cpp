b590e72ce9a746defe105ed20756c182e2381dee
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-04-07 15:15:41 +0530

    Time: 168 ms (21.26%), Space: 101.1 MB (21.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-bool solve(vector<int>& nums, int i, int t,vector<vector<int>>&dp ){
-
-    if(t==0) return true; 
-    else if(i==0 && t!=0) return false;
-
-    if(dp[i][t]!=-1) return dp[i][t];
-    
-    // pick and notPick
-    bool notTake = solve(nums,i-1, t, dp );
-    bool take=false; 
-    if(nums[i]<=t) take= solve(nums, i-1, t-nums[i], dp);
-
-    // dp[i][t] = take || notTake;
-    return dp[i][t] = take || notTake;
-    
-}
-
-
+    vector<vector<int>>dp;
+    bool solve(int i, int l, vector<int>& arr){
+      //pruning and base case:
+      if(l<0) return false; 
+      if(l==0) return true; 
+      if(i==arr.size()) return false;
+      
+      if(dp[i][l] !=-1) return dp[i][l];
+      
+      bool notTake = solve(i+1, l, arr);
+      bool take = false; 
+      if(l-arr[i] >=0) take = solve(i+1, l-arr[i], arr);
+      
+      return dp[i][l] = take || notTake; 
+      
+  }
     bool canPartition(vector<int>& nums) {
-        
         int n=nums.size();
         int sum=0;
         for(int i=0; i<n; i++) sum+=nums[i];
-         int t=sum/2; 
-         if(sum % 2 !=0) return false; 
+        if(sum % 2 !=0) return false; 
+        int t=sum/2; 
 
-          vector<vector<int>>dp(n+1, vector<int>(t+1,-1));
-         return solve(nums, n-1, t, dp);
+        dp.resize(n+1, vector<int>(t+1,-1));
+         return solve(0, t, nums);
     }
 };
