f7c060bb723be7d3db7f97d47e2d4ecb9d3e4ab2
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-04-14 01:50:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.37%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -5,9 +5,7 @@ public:
     long long binpow(long long base, long long exp, long long mod) {
         long long res = 1;
         while (exp > 0) {
-            if (exp % 2 == 1) {
-                res = (res * base) % mod;
-            }
+            if (exp % 2 == 1) res = (res * base) % mod;
             base = (base * base) % mod;
             exp /= 2;
         }
@@ -15,12 +13,14 @@ public:
     }
 
     int countGoodNumbers(long long n) {
-        long long even_positions = (n + 1) / 2; // 0, 2, 4,... (i.e., even indices)
-        long long odd_positions = n / 2;        // 1, 3, 5,... (i.e., odd indices)
+        long long op = n / 2;
+        long long ep = n - n/2;         
 
-        long long evens = binpow(5, even_positions, MOD);
-        long long odds = binpow(4, odd_positions, MOD);
+        // At even indices only 5 digits are possible: 0, 2, 4, 6, 8
+        // At odd indices only 4 digits are possible: 2, 3, 5, 7
+        long long evens = binpow(5, ep, MOD); 
+        long long odds = binpow(4, op, MOD);
 
-        return (int)((evens * odds) % MOD);
+        return ((evens * odds) % MOD);
     }
 };
