161366871cebe9676f38a9f76c35630c2603abb5
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-04-16 03:13:20 +0530

    Time: 0 ms (100%), Space: 95 MB (80.02%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int i=0; 
-        int j = 0; 
-        vector<int>nums; 
-        while(i<n && j<m){
-            if(nums1[i]<=nums2[j]) nums.push_back(nums1[i++]);
-            else nums.push_back(nums2[j++]);
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        // Ensure nums1 is the smaller array for simplicity
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
+
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high)/2; // Calculate mid index for nums1
+            int mid2 = left - mid1; // Calculate mid index for nums2
+            
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            
+            // Determine values of l1, l2, r1, and r2
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+                
+            // The partition is correct
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return max(l1, l2);
+                else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) {
+                // Move towards the left side of nums1
+                high = mid1 - 1;
+            }
+            else {
+                // Move towards the right side of nums1
+                low = mid1 + 1;
+            }
         }
-        while(i<n) nums.push_back(nums1[i++]);
-        while(j<m) nums.push_back(nums2[j++]);
-        int t = nums.size();
-        if(t%2) return (double)(nums[t/2]);
-        else {
-            int a = nums[t/2];
-            int b = nums[t/2-1];
-            return (double)(a+b)/2.0; 
-        }
-        return 0.0; // never run; 
+        
+        return 0; // If the code reaches here, the input arrays were not sorted.
     }
 };
