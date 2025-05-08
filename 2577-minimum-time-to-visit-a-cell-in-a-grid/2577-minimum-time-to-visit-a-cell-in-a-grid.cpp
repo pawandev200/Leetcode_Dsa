3277002d33638e1a9b3db6e1d6521d3b892da666
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2025-05-08 15:45:13 +0530

    Time: 888 ms (11.13%), Space: 208.9 MB (7.24%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
     int minimumTime(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
 
-        if(grid[1][0] > 1 && grid[0][1] > 1) return -1; 
-        vector<vector<int>>mint(n, vector<int>(m, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq; 
+        // Early impossible check: Can't move anywhere from (0,0)
+        if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
 
-        // starting with first cell and 0th sec: 
-        pq.push({0, 0, 0}); // time, r, c
+        vector<vector<int>> mint(n, vector<int>(m, INT_MAX));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        pq.push({0, 0, 0}); // time, row, col
         mint[0][0] = 0;
 
-        while(!pq.empty()){
-            auto curr = pq.top(); 
-            pq.pop();
-            int t = curr[0];
-            int r = curr[1];
-            int c = curr[2];
-            
-            if(r == n-1 && c == m-1) return mint[n-1][m-1];
+        while (!pq.empty()) {
+            auto curr = pq.top(); pq.pop();
+            int t = curr[0], r = curr[1], c = curr[2];
 
-            for(auto d : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int wt = max(0, (grid[nr][nc] - t));
-                    int nt = t + wt + 1;
-                    if(nt < mint[nr][nc]){
+            if (r == n - 1 && c == m - 1) return t;
+
+            for (auto d : dir) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    int wt = (grid[nr][nc] - t)%2 ? 0 : 1; 
+                    int nt = max(t + 1, grid[nr][nc] + wt);
+                    if (nt < mint[nr][nc]) {
                         mint[nr][nc] = nt;
                         pq.push({nt, nr, nc});
-                    } 
+                    }
                 }
-            } 
+            }
         }
-        return -1; // we will never encounter this situation:  
+
+        return -1;
     }
 };
