93f76a5f094dff83ad77f6e6080063f5ffb56c98
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-02-08 01:04:57 +0530

    Time: 0 ms (100%), Space: 11.9 MB (48.08%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    const static int mod = 1e9 + 7; 
+    int numSub(string s) {
+        int n = s.size();
+        int cnt = 0, total = 0; 
+        for(int i=0; i<n; i++){
+            if(s[i] == '1') cnt++; 
+            else cnt = 0; 
+            total= (total + cnt)%mod; 
+        }
+        return total; 
+    }
+};
