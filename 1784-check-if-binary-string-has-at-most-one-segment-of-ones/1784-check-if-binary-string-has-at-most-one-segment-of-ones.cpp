f86013cf099a3985976068bdce750895e88b0f31
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-06 11:29:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -6,13 +6,11 @@ public:
         int p = 0; 
         int cnt = 0; 
         for(int i=1; i<n; i++){
-            if(s[i]=='0' && s[p]=='1'){
-                cnt++; 
-            }
-            if(s[i]=='1' && s[p]=='0'){
-                cnt++; 
-            }
+            if(s[i]=='0' && s[p]=='1') cnt++; 
+            if(s[i]=='1' && s[p]=='0')cnt++; 
+
             if(cnt>1) return false; 
+            
             p = i; 
         }
         return true; 
