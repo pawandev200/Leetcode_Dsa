90ed281392925460349fffe86530cec7cac67365
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-12 20:00:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,16 @@
 # Write your MySQL query statement below
 -- self join or subquery 
-select name from Employee 
-where id in (
-    select managerId from Employee 
-    group by managerId 
-    having count(*)>=5 ) 
+-- select name from Employee 
+-- where id in (
+--     select managerId from Employee 
+--     group by managerId 
+--     having count(*)>=5 ) 
 
+-- Using join: 
+
+select l.name 
+from employee as l 
+join employee as r 
+on l.id = r.managerId
+having count(r.managerId)>=5;
 
