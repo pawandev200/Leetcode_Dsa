7767e23a08e808e0a0c75af944cf51b70cc735d1
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-14 08:54:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    
-    // String is constructed in lexicographically smallest order: 
-    // if we construct the string of size n then need to dec the cnt by one 
-    // each time if k ==0 means it is the kth smallest lexicographically string
-
-    void solve(int i, int n, int cnt, string &temp, vector<string> &ans){
-        //base case: 
-        if(i == n){ // found a new candidate, but is this kth string
-            // dec cnt globally(by &) to find the kth string from list
-            // if(--cnt ==0) ans = temp; // without storing all strings
-            ans.push_back(temp);  // by storing 
+    void solve(int i, int n, int &k, string &temp, string &ans){
+        // base case: 
+        if(k==0) return; 
+        if(i==n){
+            k--; 
+            if(k==0) ans = temp; 
             return; 
         }
-        for(char ch = 'a'; ch<='c'; ch++){
+        for(char ch='a'; ch<='c'; ch++){
             if(temp.empty() || temp.back()!=ch){
                 temp.push_back(ch);
-                solve(i+1, n, cnt, temp, ans);
+                solve(i+1, n, k, temp, ans);
                 temp.pop_back();
-                // if(cnt == 0) return; 
             }
         }
+        // return; 
     }
     string getHappyString(int n, int k) {
         string temp; 
-        // string ans="";
-        vector<string>ans;  
+        string ans = ""; 
+
         solve(0, n, k, temp, ans);
-        return k <= ans.size() ? ans[k - 1] : "";
+        return ans; 
     }
 };
