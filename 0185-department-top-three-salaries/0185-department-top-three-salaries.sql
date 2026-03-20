962f4cdbad9ebcd3b971e1464e3aa56f7c186490
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-20 19:57:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
-select d.name as Department, e.name as Employee, e.salary as Salary 
-from employee as e
-left join Department as d 
-on e.departmentId  = d.id
-where (
-    select count(distinct(e2.salary)) from employee as e2 
-    where e2.salary > e.salary and e2.departmentId = e.departmentId
-) < 3
 
--- checking each salary, will it comes to top 3 of it's dept 
--- by counting how many salary in their dept is > curr salary 
+select d.name as Department, e.name as Employee, e.salary as Salary 
+from Employee as e 
+inner join Department as d 
+on e.departmentId = d.id 
+where (
+    select count(distinct(e2.salary)) from Employee as e2
+    where e.salary < e2.salary and e.departmentId = e2.departmentId 
+) < 3; 
