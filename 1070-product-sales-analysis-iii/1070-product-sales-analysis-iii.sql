d46781a64230b4df5243a733039c148025966006
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-24 15:45:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -6,3 +6,5 @@ where (product_id, year) in (
     select product_id, min(year) from Sales
     group by product_id
 )
+
+-- checking the current row is valid or not according to requirement(first year)
