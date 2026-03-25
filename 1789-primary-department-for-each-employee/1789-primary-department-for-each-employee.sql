f504cc57f65971a1080d3b5672e093cea1c515e7
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-25 13:13:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -2,11 +2,12 @@
 
 -- select employee_id, department_id 
 -- from Employee 
+-- group by employee_id 
 -- having primary_flag='Y' or COUNT(employee_id) = 1 
 
 select employee_id, department_id 
 from Employee 
-where primary_flag='Y' 
+where primary_flag='Y'  -- since only one y for a employee
  or employee_id in (
     select employee_id from Employee 
     group by employee_id 
