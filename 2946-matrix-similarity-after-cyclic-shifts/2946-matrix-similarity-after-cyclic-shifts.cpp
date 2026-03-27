b8912e09ae588f9752332ca985cd7a63d72319a0
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-27 13:11:49 +0530

    Time: 3 ms (30.43%), Space: 31.3 MB (49.69%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        k = k%n; 
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i%2==0){ // shift left 
+                   if(mat[i][j] != mat[i][(j+k)%n]) return false; 
+                }
+                else { // shift right
+                    if(mat[i][j] != mat[i][(j-k + n) % n]) return false; 
+                }
+            }
+        }
+        return true; 
+    }
+};
