31c960dc23e749735b12f8d29de6d97843c7a7ee
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-03-30 16:52:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -3,19 +3,20 @@ public:
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
 
-        unordered_map<char, int>omp; 
-        unordered_map<char, int>emp;
+        vector<int>omp(26, 0); 
+        vector<int>emp(26, 0); 
 
         for(int i=0; i<n; i++){
-            if(i%2==0) emp[s1[i]] ++; 
-            else omp[s1[i]] ++; 
+            if(i%2==0) emp[s1[i] - 'a'] ++; 
+            else omp[s1[i] - 'a'] ++; 
         } 
 
         for(int i=0; i<n; i++){
-            if(i%2==0) emp[s2[i]] --; 
-            else omp[s2[i]] --;
-            if(emp[s2[i]] < 0) return false; 
-            if(omp[s2[i]] < 0) return false; 
+            if(i%2==0) emp[s2[i]- 'a'] --; 
+            else omp[s2[i]- 'a'] --;
+            
+            if(emp[s2[i]- 'a'] < 0) return false; 
+            if(omp[s2[i]- 'a'] < 0) return false; 
         } 
 
         return true; 
