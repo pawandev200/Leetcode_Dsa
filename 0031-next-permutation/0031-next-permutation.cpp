138162c6beb7d9c3985482558727c5a802ffd05b
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-02 11:43:16 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.91%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n-2; 
+
+        // first decreasing num from right: [1, 3, 5, 4, 2]
+        while(i>=0 && nums[i] >= nums[i+1]) i--; 
+
+        // if found, then swap with just greater val
+        if(i>=0){
+            int j = n-1; 
+            while(nums[i] >= nums[j]) j--;
+            swap(nums[i], nums[j]); 
+        }
+
+        // reverse the right part to get next: 
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
