d26c62348e99af89cca5faeb580c0a5691dbb481
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-07 16:48:58 +0530

    Time: 174 ms (78.03%), Space: 13.9 MB (83.82%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool isprime(int num){
-        for(int i=2; i*i <= num; i++){
-            if(num%i ==0) return false; 
-        }
-        return true; 
-    }
+   
     vector<int> closestPrimes(int l, int r) {
-        vector<int>prime; 
-        for(int i=max(2,l); i<=r; i++){ // skipping the 1: bcoz it is not prime
-            if(isprime(i)) prime.push_back(i);
-        }
-        // for(int i=0; i<prime.size(); i++) cout<<prime[i]<<endl; 
-
-        if(prime.size() == 0) return {-1,-1};
-        int mini = INT_MAX;
-        int a = -1, b = -1;  
-        for(int i=0; i<prime.size()-1; i++){
-            if(mini > prime[i+1] - prime[i]){
-                mini = prime[i+1] - prime[i];
-                a = prime[i];
-                b = prime[i+1];
+        vector<bool>isprime(r+1, 1);
+        isprime[0] = isprime[1] = 0; 
+        for(int i=2; i*i<=r; i++){
+            if(isprime[i]){ // means i is prime
+                for(int j = i*i; j<=r; j+=i) isprime[j] = 0; 
             }
         }
-        return {a,b}; 
+        // now chcking in the range:
+        int mindiff = INT_MAX;
+        pair<int, int> temp = {-1, -1};  
+        int prev = -1; 
+        for(int i=l; i<=r; i++){
+            if(isprime[i] ){
+                int diff = i - prev; 
+                if(prev!=-1 && diff < mindiff){
+                    mindiff = diff; 
+                    temp = {prev, i};
+                }
+                prev = i; 
+            }
+        }
+        return {temp.first, temp.second}; 
     }
 };
