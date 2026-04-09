433d38a5176b05be6dfa5c5913ce998b45fffa67
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-09 15:04:23 +0530

    Time: 31 ms (34.29%), Space: 26.2 MB (32.06%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // den = 1 to n
+    // num = 1 to n-1  ajdust in such a way so fraction: (0, 1)
+    vector<string> simplifiedFractions(int n) {
+        // if(n <= 1) return {" "};
+        vector<string>ans; 
+        for(int i=1; i<n; i++){ // num
+            for(int j = i+1; j<=n; j++){ // den
+                double val = (double)i/j; 
+                if(__gcd(i, j)==1){
+                    string num = to_string(i);
+                    string den = to_string(j);
+                    ans.push_back(num + "/" + den);
+                }
+            }
+        }
+        return ans; 
+    }
+};
