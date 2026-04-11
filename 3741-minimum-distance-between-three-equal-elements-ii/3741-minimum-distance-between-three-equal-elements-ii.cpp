3d49d3433f464c53c9a691612c6bf89d835b4a50
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-11 08:56:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
         int n = nums.size();
-        map<int, vector<int>>mp; // num, idxs
-        for(int i=0; i<n; i++) mp[nums[i]].push_back(i);
-
+        unordered_map<int, pair<int, int>>mp; // num, last two idxs
         int ans = INT_MAX; 
-        for(auto &it: mp){
-            if(it.second.size()>=3){
-                for(int i=2; i<it.second.size(); i++){
-                    int a = it.second[i-2];
-                    int b = it.second[i-1];
-                    int c = it.second[i];
-                    int dist = abs(a-b) + abs(b-c) + abs(c-a);
-                    ans = min(ans, dist);
-                }
-            }
+
+        for(int i=0; i<n; i++){
+            int cv = nums[i];
+            if(mp.count(cv)){
+                int f = mp[cv].first; 
+                int s = mp[cv].second; 
+                if(f!=-1) ans = min(ans, 2*(i-f));
+
+                mp[cv] = {s, i};
+            } else mp[cv] = {-1, i};
         }
+
         return ans == INT_MAX ? -1 : ans; 
     }
 };
