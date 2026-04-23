c5e7d777a35e12c53426f7763dee9633f63cb993
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-23 23:08:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<long long> distance(vector<int>& nums) {
-    int n = nums.size();
-    vector<long long> ans(n, 0);
-    unordered_map<int, vector<int>> mp;
+        int n = nums.size(); 
+        unordered_map<int, vector<int>>mp; // num, indices; 
+        for(int i=0; i<n; i++) mp[nums[i]].push_back(i);
 
-    for (int i = 0; i < n; i++) mp[nums[i]].push_back(i);
+        vector<long long>ans(n, 0); 
+        for(const auto& [num, posv]: mp){
+            int m = posv.size();
+            if(m==1) continue;  // already intialized with 0
 
-    for (const auto& [val, posv] : mp) {
-        long long ts = 0;
-        for (int idx : posv) ts += idx;
+            long long ts = 0; 
+            for(int k=0; k<m; k++) ts+=posv[k];
 
-        long long lps = 0;
-        int m = posv.size();
-        
-        for (int i = 0; i < m; i++) {
-            int cidx = posv[i];
-            long long ls = 1LL*cidx*i - lps; 
-            long long rs = (ts - lps - cidx) - 1LL*(m-i-1)*cidx; 
+            long long lps = 0; 
+            for(int k=0; k<m; k++){
+                int i = posv[k];
+                long long ls = 1LL*i*k - lps; 
+                long long rs = (ts - lps - i) - 1LL*(m-k-1)*i; 
 
-            ans[cidx] = ls + rs; 
-            lps +=cidx; 
+                ans[i] = ls + rs; 
+                lps +=i; 
+            }
         }
+        return ans;
     }
-    return ans;
-}
 };
