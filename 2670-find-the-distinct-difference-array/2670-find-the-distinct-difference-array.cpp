8ad89cb02aee4a76e1a4382bc1f14c6df51f12d0
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-25 16:23:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int>diff(n, 0);
-        int lcnt = 0; 
-        for(int i=0; i<n; i++){
-            unordered_set<int>st; 
-            for(int k=0; k<=i; k++) st.insert(nums[k]);
-            int lcnt = st.size();
-            st.clear();
+        vector<int>pf(n, 0), sf(n, 0), diff(n, 0);
 
-            for(int k=i+1; k<n; k++) st.insert(nums[k]);
-            int rcnt = st.size();
-            diff[i] = lcnt - rcnt; 
-            st.clear();
+        unordered_set<int>st; 
+        for(int i=0; i<n; i++){
+            st.insert(nums[i]);
+            pf[i] = st.size();
+        }
+
+        st.clear();
+        for(int i=n-1; i>=0; i--){
+            st.insert(nums[i]);
+            sf[i] = st.size();
+        }
+
+        for(int i=0; i<n; i++){
+            int rcnt = (i==n-1 ? 0 : sf[i+1]);
+            diff[i] = pf[i] - rcnt;
         }
         return diff; 
     }
