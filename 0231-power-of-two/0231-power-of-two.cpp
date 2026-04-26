71f66c45c240951d48ea464fafdf4c08504f62d5
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-26 13:35:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        // for(int i=0; i<n; i++){
-        //     if(1<<i == n) return true; 
-        // }
-        // return false; 
-        if(n<=0) return false; 
-        return !(n & (n-1));
+        for(int i=0; i<n; i++){
+            if(1<<i == n) return true; 
+            else if(1<<i > n) return false;
+        }
+        return false; 
     }
 };
