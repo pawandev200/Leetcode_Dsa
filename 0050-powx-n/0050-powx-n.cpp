682c4244bcd7ea434032b4fde96891f9131a95a8
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-29 14:53:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-   double binpow(double b, int expo){
+   double binpow(double b, int n){
+    long long expo = n; 
     if(expo < 0){
         b = 1 / b; 
         expo = - expo; 
