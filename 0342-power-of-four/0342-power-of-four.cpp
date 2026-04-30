cb7e662225f5e9d8340078b800f0561d307ceab5
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-30 10:37:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -6,9 +6,14 @@ public:
         // while(n%4==0) n=n/4; 
         // return n == 1; 
 
-        // bit technique: 
-        if(n<=0) return false; 
-        return !(n & (n-1)) && (n-1)%3 == 0; 
+        // Math: 
+        if (n <= 0) return false;
+        double x = log2(n);
+        return floor(x) == x && ((int)x % 2 == 0);
+        
+        // // bit technique: 
+        // if(n<=0) return false; 
+        // return !(n & (n-1)) && (n-1)%3 == 0; 
 
     }
 };
