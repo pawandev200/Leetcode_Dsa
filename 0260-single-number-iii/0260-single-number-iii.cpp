4686eb71274c3e36e77ad5c62ea97fc1b878a1c4
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-30 23:50:12 +0530

    Time: 0 ms (100%), Space: 13.9 MB (54.65%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int n = nums.size();
+        int x = 0;
+        for (int num : nums) x ^= num;
 
-        int x = 0; 
-        for(const int& num: nums) x^=num; 
-
-        int mask = x & (-x);
+        long long mask = (long long)x & -(long long)x;
 
         int a = 0, b = 0;
 
-        //  Divide into two groups and XOR separately
         for (int num : nums) {
-            if (num & mask) a ^= num; 
-            else b ^= num;   
+            if (num & mask) a ^= num;
+            else b ^= num;
         }
 
         return {a, b};
