30da17a4860e967ff560cfb1d3b5f00fd12729e7
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-30 23:33:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int n = nums.size();
-        // int xr = 0; 
-        // for(const int& num: nums) xr^=num; 
 
-        unordered_map<int, int>mp; // num, freq; 
-        for(const int& num: nums) mp[num]++; 
+        int x = 0; 
+        for(const int& num: nums) x^=num; 
 
-        vector<int>ans; 
-        for(const auto& [val, cnt]: mp){
-            if(cnt==1) ans.push_back(val);
+        int mask = x & (-x);
+
+        int a = 0, b = 0;
+
+        //  Divide into two groups and XOR separately
+        for (int num : nums) {
+            if (num & mask) a ^= num; 
+            else b ^= num;   
         }
 
-        return ans; 
+        return {a, b};
     }
 };
