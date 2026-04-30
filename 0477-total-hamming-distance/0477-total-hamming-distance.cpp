af3414bcd0b5ceb7a9799f64cb22d65ad5349ab5
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-30 19:26:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int gethdist(int x){
-        return __builtin_popcount(x);
-    }
+    
     int totalHammingDistance(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0; 
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                int x = nums[i] ^ nums[j]; 
-                cnt+=gethdist(x);
+        // Approach: at each bit count the diff and sum it: 
+        for(int bit=0; bit<32; bit++){
+            int ones = 0; 
+            for(const int& num: nums){
+                if(num & (1<<bit)) ones++;  // check the bit pos 
             }
+            int zeros = n - ones; 
+            cnt += ones * zeros; 
         }
         return cnt; 
     }
