7e6a1fcf270317e5669d6ec0034613b28f629cdb
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-04-30 23:06:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+// Track bits using two variables: ones (seen once) and twos (seen twice).
+// Each new number updates these states using XOR; when a bit appears the third time, it gets removed from both.
+// After processing all numbers, only the bits of the unique element remain in ones.
+
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int>mp; // num, cnt; 
-        for(const int& num: nums) mp[num]++; 
+        int ones = 0, twos = 0;
 
-        for(const auto& it: mp){
-            int cnt = it.second; 
-            if(cnt==1) return it.first; 
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
-        return -1; 
+
+        return ones;
     }
 };
