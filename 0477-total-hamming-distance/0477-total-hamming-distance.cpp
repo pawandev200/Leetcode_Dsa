bf5fea3e213a1f55db60edb3418b4e65ad66edcb
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-01 18:47:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -4,13 +4,13 @@ public:
     int totalHammingDistance(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0; 
-        // Approach: at each bit count the diff and sum it: 
+        // Approach: at each bit count the diff bit in all ele and sum it: 
         for(int bit=0; bit<32; bit++){
-            int ones = 0; 
+            int ones = 0, zeros = 0; 
             for(const int& num: nums){
                 if(num & (1<<bit)) ones++;  // check the bit pos 
+                else zeros++; 
             }
-            int zeros = n - ones; 
             cnt += ones * zeros; 
         }
         return cnt; 
