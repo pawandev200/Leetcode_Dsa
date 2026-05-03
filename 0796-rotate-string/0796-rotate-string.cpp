bbdb374ee04d8cdf201129f01af9dad3d323a0d0
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-03 13:44:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+
+        // // goals should be substring of s + s: 
+        // if(s.size() != goal.size()) return false;
+        // return (s + s).find(goal) != string::npos;
+
         int n = s.size();
         if(n != goal.size()) return false;
         if(s == goal) return true;  
 
-        // checking each shift: 
-        for(int i=0; i<n; i++){
-            s += s[i];
-            string sub = s.substr(i+1, n+1);
-            if(sub == goal) return true; 
+        // checking each shift: shifting by 1 , 2, ... n-1
+        for(int i = 0; i < n; i++) {
+            string rotated = s.substr(i, n) + s.substr(0, i);
+            if(rotated == goal) return true;
         }
-        return false; 
+        return false;
     }
 };
