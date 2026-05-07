35be57944ab460c3643eff6df288981fcb5a9dd9
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-07 10:51:05 +0530

    Time: 0 ms (100%), Space: 28.2 MB (27.37%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ans = -1; 
+        int cnt = 0; // no majority, no minority so curr num is majority assume
+        for(const int& num: nums){
+            if(cnt==0) ans = num; 
+
+            if(num==ans) cnt++; 
+            else cnt--; 
+        }
+        return ans; 
+    }
+};
