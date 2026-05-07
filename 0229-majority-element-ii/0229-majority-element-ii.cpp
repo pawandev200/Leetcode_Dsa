e4b818ee65aed5b21e563e0070e1d580d44b6603
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-07 11:26:01 +0530

    Time: 0 ms (100%), Space: 23.6 MB (63.74%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int f = 0, s = 0; 
+        int fc = 0, sc = 0; 
+        for(const int& num: nums){ 
+            if(f == num) fc++; 
+            else if(s == num) sc++; 
+            else if (fc == 0) {f = num; fc++;} 
+            else if(sc ==0) {s = num; sc++;}
+            else {
+                fc--; 
+                sc--; 
+            }
+        }
+        if(f == s) return {f};
+
+        fc = 0;
+        sc = 0; 
+        for(const int& num: nums){
+            if(num==f) fc++; 
+            else if(num==s) sc++; 
+        }
+        vector<int>ans;
+        int th = nums.size()/3; 
+
+        if(fc > th) ans.push_back(f);
+        if(sc > th) ans.push_back(s);
+
+        return ans; 
+    }
+};
