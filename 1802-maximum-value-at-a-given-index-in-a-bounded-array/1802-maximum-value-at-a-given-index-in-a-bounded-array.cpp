aaa4468c2ad8eee95823ffe4937e9926bff9e7d2
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-12 16:04:48 +0530

    Time: 0 ms (100%), Space: 8.2 MB (2.93%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -5,19 +5,14 @@ public:
 
         // enough decreasing numbers
         if(maxi > cnt){
-
             long long low = maxi - cnt;
-
             return (maxi - 1 + low) * 1LL * cnt / 2;
         }
 
         // reaches 1
         else{
-
             long long aps = 1LL * (maxi - 1) * maxi / 2;
-
             long long ones = cnt - (maxi - 1);
-
             return aps + ones;
         }
     }
@@ -25,12 +20,8 @@ public:
     bool check(int mid, int n, int idx, int maxs){
 
         long long sum = mid;
-
-        // left side
-        sum += getsum(idx, mid);
-
-        // right side
-        sum += getsum(n - idx - 1, mid);
+        sum += getsum(idx, mid); // left side
+        sum += getsum(n - idx - 1, mid); // right side
 
         return sum <= maxs;
     }
