bd32f87ed00d47b85d1fe675bd0774e15e6c6920
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-13 14:38:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,9 +4,11 @@ public:
         int ans = -1; 
         int cnt = 0; // no majority, no minority so curr num is majority assume
         for(const int& num: nums){
-            if(cnt==0) ans = num; 
-
-            if(num==ans) cnt++; 
+            if(cnt==0){
+                ans = num; 
+                cnt++; 
+            }
+            else if(num==ans) cnt++; 
             else cnt--; 
         }
         return ans; 
