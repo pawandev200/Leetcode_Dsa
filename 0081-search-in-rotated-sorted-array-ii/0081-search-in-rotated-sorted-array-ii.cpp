8e1fe571cc15af84c9a463b2fc9a2a1ac142525a
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 13:00:19 +0530

    Time: 0 ms (100%), Space: 18.2 MB (28.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
     int pivot(vector<int>& nums, int l, int r) {
-        while(l < r) {
-            
-            while(l < r && nums[l] == nums[l+1]) l++;
-            
-            while(r < l && nums[r] == nums[r-1]) r--;
-            
-            int mid = l + (r-l)/2;
-            
-            if(nums[mid] <= nums[r]) { //sorted part
-                r = mid; //possibly my pivot
-            } else {
-                l = mid+1;
+        int n = nums.size();
+        int pt = 0;
+
+        // remove duplicates from beginning
+        while (l < n && nums[l] == nums[n - 1])
+            l++;
+
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (nums[mid] <= nums[n - 1]) {
+                pt = mid;
+                r = mid - 1; // pivot can be on left side
+            }
+            else {
+                l = mid + 1; // pivot lies on right side
             }
         }
-        
-        return r;
+
+        return pt;
     }
     
-    bool binarySearch(vector<int>& nums, int l, int r, int& target) {
+    bool binarySearch(vector<int>& nums, int l, int r, int& t) {
         while(l <= r) {
             int mid = l + (r-l)/2;
             
-            if(nums[mid] == target) return true;
-            
-            if(nums[mid] < target)
-                l = mid+1;
-            else
-                r = mid-1;
+            if(nums[mid] == t) return true;
+            if(nums[mid] < t) l = mid+1;
+            else r = mid-1;
         }
         
         return false;
     }
     
-    bool search(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int t) {
         int n = nums.size();
-        
         
         int p = pivot(nums, 0 , n-1);
         cout <<"p = " << p << endl;
-        if(binarySearch(nums, 0, p-1, target)) {
-            return true;
-        }
-        
-        return binarySearch(nums, p, n-1, target);
+
+        if(binarySearch(nums, 0, p-1, t)) return true;
+
+        return binarySearch(nums, p, n-1, t);
             
     }
 };
