9f4b3bca47dd3b2be93605af617b4eacd4046b43
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-17 01:05:25 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.39%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
-    // for each pair, 1st ele takes even pos and 2nd ele takes odd pos: but 
-    // this pattern get reversed after single ele appeared 
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        int l = 0; 
-        int h = n-1; 
-        while(l<h){
-            int mid = l + (h-l)/2;
-            // if mid is even, dup should at it's next idx,
-            //  if odd,  dup should be at it's prev idx.
-            if(mid%2==1) mid--; // making even so no need to check for mid -1 
-            if(nums[mid] == nums[mid+1]) l = mid + 2; // pattern is okay in lh 
-            else h = mid; 
-        }
-        return nums[l];
+        int xr = 0; 
+        for(const int& num: nums) xr ^= num; 
+
+        return xr; 
     }
 };
