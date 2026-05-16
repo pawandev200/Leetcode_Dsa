146c50e0f123a83457bf706ee1410560cd75577e
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 18:10:42 +0530

    Time: 0 ms (100%), Space: 12.7 MB (38.52%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    // Slope dir gives guaranteed peak existence. No need if array is sorted,
+/* 
+It's about finding local maxima, return any maxima: chance of it's existance, can be checked by slope dir. peak: nums[i] > nums[i-1] && nums[i] > nums[i+1] 
+slope dir: alone any one can determine: let go with nums[i] > nums[i+1]
+Slope dir gives guaranteed peak existence. No need if array is sorted
+*/
+
+// find mid check if it can peak or not, if not safely go left or right part
+
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
         int l = 0; 
         int r = n-1;
         int ans = n-1; 
 
-        while (l < n && nums[l] == nums[n - 1]) l++;
         while(l<=r){
             int mid = (l+r)/2;
 
@@ -17,10 +23,23 @@ public:
     
             if (left && right) return mid;  // peak found
 
+            // plateau case: Can't be here, it's given
+            // [1,2,2,2,3] At middle: 2 == 2 => can't discard either side
+            // handling plateau depens or problems, strict maxima or not, etc 
+
             // increasing slope -> peak on right
-            if (mid < n - 1 && nums[mid] < nums[mid + 1])  l = mid + 1;
+            else if (mid + 1 < n && nums[mid] < nums[mid + 1]) l = mid + 1;
             else r = mid - 1;
         }
         return ans; 
     }
 };
+ 
+/*  Out of scope of this question: 
+
+If nums[mid] == nums[mid + 1]: we can't discard either side. then we can shrink the search space: l++, r-- until differ
+[1,2,2,2,3] At middle: 2 == 2 how would you handle: 
+[3,2,2,2,1]
+
+if(mid < n - 1 && nums[mid] == nums[mid + 1]) then r = mid-1, l++, or r--, etc depends on problem like: strict maxima or not, 
+*/
