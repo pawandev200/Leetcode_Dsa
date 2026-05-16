45fe81a3b99520aef86baf748070e7ab936d9afe
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 15:23:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (94.64%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+    // Slope dir gives guaranteed peak existence. No need if array is sorted,
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int l =0; 
-        int r = n-1; 
-        while(l<r){
-            int mid = (l+r)/2; 
-            if(nums[mid] < nums[mid+1]) l = mid + 1; 
-            else r = mid; 
+        int l = 0; 
+        int r = n-1;
+        int ans = n-1; 
+        while(l<=r){
+            int mid = (l+r)/2;
+
+            // checking mid is peak or not: 
+            bool left = (mid == 0 || nums[mid] > nums[mid - 1]);
+            bool right = (mid == n - 1 || nums[mid] > nums[mid + 1]);
+    
+            if (left && right) return mid;  // peak found
+
+            // increasing slope -> peak on right
+            if (mid < n - 1 && nums[mid] < nums[mid + 1])  l = mid + 1;
+            else r = mid - 1;
         }
-        return l; // l == r, which is the peak index
+        return ans; 
     }
 };
