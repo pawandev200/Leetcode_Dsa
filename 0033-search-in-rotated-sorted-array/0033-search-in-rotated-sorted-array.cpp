025688652ae1af295fee2429c2d34f74a6286233
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 13:43:39 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,62 @@
 class Solution {
 public:
-    int check(int pos, vector<int>&nums){
+    bool check(int mid, vector<int>&nums){
         int n = nums.size();
-        if(nums[pos] < nums[0]) return 1; 
-        return 0; 
+        if(nums[mid] < nums[n-1]) return true; 
+        return false; 
+    }
+
+    int binarySearch(vector<int>& nums, int l, int r, int& t) {
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] == t) return mid;
+            if(nums[mid] < t) l = mid+1;
+            else r = mid-1;
+        }
+        
+        return -1;
     }
     int search(vector<int>& nums, int t) {
         int n = nums.size();
         int l = 0;
         int r = n-1;
-        int por = nums[0];
+        int pt = nums[0];
 
         // remove duplicates from beginning
         while (l < n && nums[l] == nums[n-1]) l++;
 
         while (l <= r) {
-            int mid = l + ((r - l) >> 1);
+            int mid = l + (r - l)/2;
             if (nums[mid] <= nums[n-1]) {
-                por = mid;
-                r = mid - 1; // minimum can be on l side
+                pt = mid;
+                r = mid - 1; 
             }
-            else  l = mid + 1; // minimum lies on r side
-        }
-         
-        // Now for searching the target: Bs on correct half:
-
-        // if roation: nums[por] < nums[0]: then lp > rp
-        // left part: 0 -> por-1; 
-        // right part: por -> n-1;
-
-        if (t >= nums[0]){ // means target in the left part of the array
-            l = 0;
-            r = (por == 0 ? n - 1 : por - 1); // if no rotation
-        } else {
-            l = por, r = n - 1;
+            else l = mid + 1; 
         }
 
-        int ans = -1; // default answer
-        while(l<=r){
-            int mid = l + (r - l)/2; 
+        int lv = binarySearch(nums, 0, pt-1, t);
+        int rv = binarySearch(nums, pt, n-1, t);
 
-            if(nums[mid] == t) return mid; 
-            else if(nums[mid] < t) l = mid + 1; 
-            else r = mid - 1; 
-        }
-        return -1;  
+        if(lv != -1) return lv;
+        return rv == -1 ? -1 : rv;
+
+
+        // if (t >= nums[0]){ // means target in the left part of the array
+        //     l = 0;
+        //     r = (pt == 0 ? n - 1 : pt - 1); // if no rotation
+        // } else {
+        //     l = pt, r = n - 1;
+        // }
+
+        // int ans = -1; // default answer
+        // while(l<=r){
+        //     int mid = l + (r - l)/2; 
+
+        //     if(nums[mid] == t) return mid; 
+        //     else if(nums[mid] < t) l = mid + 1; 
+        //     else r = mid - 1; 
+        // }
+        // return ans;  
     }
 };
