eb3aee3a2967ae5fa4ad0429193c8c86c7916ab8
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 12:21:32 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,48 @@
 class Solution {
 public:
-    int check(int pos, vector<int>&arr){
-        if(arr[pos] < arr[0]) return 1; 
+    int check(int pos, vector<int>&nums){
+        int n = nums.size();
+        if(nums[pos] < nums[0]) return 1; 
         return 0; 
     }
     int search(vector<int>& nums, int t) {
         int n = nums.size();
-        // first finding out the pivot(pt of rotation)
-        int l = 0; 
-        int h = n-1;
-        int pot = 0; // default val of rotation 
-        while(l<=h){
-            int mid = l + (h-l)/2; 
-            if(check(mid, nums) == 1){
-                pot = mid; 
-                h = mid -1; 
+        int l = 0;
+        int r = n-1;
+        int por = nums[0];
+
+        // remove duplicates from beginning
+        while (l < n && nums[l] == nums[n-1]) l++;
+
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[mid] <= nums[n-1]) {
+                por = mid;
+                r = mid - 1; // minimum can be on l side
             }
-            else l = mid + 1; 
+            else  l = mid + 1; // minimum lies on r side
+        }
+         
+        // Now for searching the target: Bs on correct half:
+
+        // if roation: nums[por] < nums[0]: then lp > rp
+        // left part: 0 -> por-1; 
+        // right part: por -> n-1;
+
+        if (t >= nums[0]){ // means target in the left part of the array
+            l = 0;
+            r = (por == 0 ? n - 1 : por - 1); // if no rotation
+        } else {
+            l = por, r = n - 1;
         }
 
-        // for searching the target: Bs on correct half: 
-        if (t >= nums[0]) {
-            l = 0, h = (pot == 0 ? n - 1 : pot - 1);
-        } else {
-            l = pot, h = n - 1;
-        } 
         int ans = -1; // default answer
-        while(l<=h){
-            int mid = (l + h)/2; 
+        while(l<=r){
+            int mid = l + (r - l)/2; 
 
             if(nums[mid] == t) return mid; 
             else if(nums[mid] < t) l = mid + 1; 
-            else h = mid - 1; 
+            else r = mid - 1; 
         }
         return -1;  
     }
