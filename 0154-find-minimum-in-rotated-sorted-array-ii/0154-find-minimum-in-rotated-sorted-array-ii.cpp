34f8079f21f1fc914bcc87991654c37cf440a3da
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 12:04:39 +0530

    Time: 0 ms (100%), Space: 16.1 MB (90%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-
-    bool check(int mid, int l, int r, vector<int>& nums) {
-        // If mid equals right, we can't determine which side
-        // Return false to handle this case
-        if (nums[mid] == nums[r]) {
-            return false;
-        }
-
-        // minimum is in right half (right half is sorted)
-        if (nums[mid] < nums[r]) {
-            return true;
-        }
-
-        // minimum is in left half (left half has rotation point)
-        return false;
-    }
-
     int findMin(vector<int>& nums) {
+
         int n = nums.size();
 
         int l = 0;
-        int r = n - 1;
+        int r = n-1;
+        int ans = nums[0];
 
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            
-            if (check(mid, l, r, nums)) {
-                // Right half is sorted, minimum is in [l, mid]
-                r = mid;
+        // remove duplicates from beginning
+        while (l < n && nums[l] == nums[n-1])
+            l++;
+
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (nums[mid] <= nums[n-1]) {
+                ans = nums[mid];
+                r = mid - 1; // minimum can be on l side
             }
             else {
-                // Either left half is sorted or we have duplicates
-                // Move left pointer forward to skip duplicates
-                if (nums[mid] == nums[r]) {
-                    r--;  // Shrink from right when we have duplicates
-                } else {
-                    l = mid + 1;  // Move left when left half has rotation
-                }
+                // minimum lies on r side
+                l = mid + 1;
             }
         }
 
-        return nums[l];
+        return ans;
     }
 };
