67b8a8cbab89a66b50be50d97296dd307ad8056f
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 12:43:43 +0530

    Time: 0 ms (100%), Space: 17.9 MB (61.6%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-    // Pivot check function: compare element at pos with the last element.
-
-    int check(int pos, vector<int>& arr, int h) {
-        if(arr[pos] < arr[h]) return 1;
-        else if(arr[pos] > arr[h]) return 0;
-        else return -1;  //if they are equal, used to shring search space
-    }
-    
-    bool search(vector<int>& nums, int t) {
-        int n = nums.size();
-        if(n == 0) return false;
-        
-        int l = 0, h = n - 1, por = 0;
-        while(l <= h) {
-            int mid = l + (h - l) / 2;
-            int condition = check(mid, nums, n - 1);
-            if(condition == 1) {        
-                por = mid;              
-                h = mid - 1;
-            } else if(condition == 0) { 
-                l = mid + 1;
-            } else {  // nums[mid] == nums[n-1]
-                h--;                   
+    int pivot(vector<int>& nums, int l, int r) {
+        while(l < r) {
+            
+            while(l < r && nums[l] == nums[l+1]) l++;
+            
+            while(r < l && nums[r] == nums[r-1]) r--;
+            
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] <= nums[r]) { //sorted part
+                r = mid; //possibly my pivot
+            } else {
+                l = mid+1;
             }
         }
-
-        if(nums[por] >= nums[0]) por = 0; // no rotation
         
-        // Segment1: indices [0, por - 1]  and Segment2: indices [por, n - 1]
-        if(t >= nums[0]) {        
-            l = 0;
-            h = (por == 0 ? n - 1 : por - 1);
-        } else {                   
-            l = por;
-            h = n - 1;
+        return r;
+    }
+    
+    bool binarySearch(vector<int>& nums, int l, int r, int& target) {
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] == target) return true;
+            
+            if(nums[mid] < target)
+                l = mid+1;
+            else
+                r = mid-1;
         }
         
-        // Standard binary search using while(l <= h)
-        while(l <= h) {
-            int mid = l + (h - l) / 2;
-            if(nums[mid] == t) return true;
-            else if(nums[mid] < t) l = mid + 1;
-            else h = mid - 1;
-        }
         return false;
+    }
+    
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        
+        int p = pivot(nums, 0 , n-1);
+        cout <<"p = " << p << endl;
+        if(binarySearch(nums, 0, p-1, target)) {
+            return true;
+        }
+        
+        return binarySearch(nums, p, n-1, target);
+            
     }
 };
