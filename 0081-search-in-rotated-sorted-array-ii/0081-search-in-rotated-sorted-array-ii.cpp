e52b5d7f898bca7a50acd0db7aadb75b3effa4ce
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 13:30:34 +0530

    Time: 0 ms (100%), Space: 18 MB (61.6%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,51 +1,49 @@
 class Solution {
 public:
-    // int pivot(vector<int>& nums, int l, int r) {
-    //     int n = nums.size();
-    //     int pt = 0;
-
-    //     // remove duplicates from beginning
-    //     while (l < n && nums[l] == nums[n - 1]) l++;
-
-    //     while (l <= r) {
-    //         int mid = l + ((r - l) >> 1);
-
-    //         if (nums[mid] <= nums[n - 1]) {
-    //             pt = mid;
-    //             r = mid - 1; // pivot can be on left side
-    //         }
-    //         else {
-    //             l = mid + 1; // pivot lies on right side
-    //         }
-    //     }
-
-    //     return pt;
-    // }
-
     int pivot(vector<int>& nums, int l, int r) {
-    int pt = 0;
+        int n = nums.size();
+        int pt = 0;
 
-    while (l < r) {
+        // remove duplicates from beginning
+        while (l < n && nums[l] == nums[n - 1]) l++;
 
-        // handle duplicates
-        while (l < r && nums[l] == nums[l + 1]) l++;
-        while (l < r && nums[r] == nums[r - 1]) r--;
+        while (l <= r) {
+            int mid = l + ((r - l) >> 1);
 
-        int mid = l + (r - l) / 2;
-
-        // right half is sorted, pivot may be mid or left side
-        if (nums[mid] <= nums[r]) {
-            pt = mid;
-            r = mid;
+            if (nums[mid] <= nums[n - 1]) {
+                pt = mid;
+                r = mid - 1; // pivot can be on left side
+            }
+            else {
+                l = mid + 1; // pivot lies on right side
+            }
         }
-        else {
-            // pivot lies on right side
-            l = mid + 1;
-        }
+
+        return pt;
     }
 
-    return l; // or return pt;
-}
+// int pivot(vector<int>& nums, int l, int r) {
+//     int pt = 0; 
+//     while(l <= r) {
+        
+//         // Skip duplicates from left and right: 
+//         while(l < r && nums[l] == nums[l+1]) l++;
+//         while(r > l && nums[r] == nums[r-1]) r--;
+        
+//         int mid = l + (r - l) / 2;
+        
+//         if(nums[mid] <= nums[r]) { // sorted part, min is in [l, mid]
+//             if (nums[mid] < nums[pt]) {
+//                 pt = mid;  // Update only if we found smaller value
+//             }
+//             r = mid - 1;  // Search left
+//         } else {  // unsorted part, min is in [mid+1, r]
+//             l = mid + 1;  // Search right
+//         }
+//     }
+    
+//     return pt;
+// }
     
     bool binarySearch(vector<int>& nums, int l, int r, int& t) {
         while(l <= r) {
