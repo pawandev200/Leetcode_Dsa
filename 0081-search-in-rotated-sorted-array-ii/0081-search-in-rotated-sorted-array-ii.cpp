7358fc49fd3925c512c4affbd5f0bb2ffa9d87a8
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-16 13:01:59 +0530

    Time: 0 ms (100%), Space: 18.1 MB (28.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
+    // int pivot(vector<int>& nums, int l, int r) {
+    //     int n = nums.size();
+    //     int pt = 0;
+
+    //     // remove duplicates from beginning
+    //     while (l < n && nums[l] == nums[n - 1]) l++;
+
+    //     while (l <= r) {
+    //         int mid = l + ((r - l) >> 1);
+
+    //         if (nums[mid] <= nums[n - 1]) {
+    //             pt = mid;
+    //             r = mid - 1; // pivot can be on left side
+    //         }
+    //         else {
+    //             l = mid + 1; // pivot lies on right side
+    //         }
+    //     }
+
+    //     return pt;
+    // }
+
     int pivot(vector<int>& nums, int l, int r) {
-        int n = nums.size();
-        int pt = 0;
+    int pt = 0;
 
-        // remove duplicates from beginning
-        while (l < n && nums[l] == nums[n - 1])
-            l++;
+    while (l < r) {
 
-        while (l <= r) {
-            int mid = l + ((r - l) >> 1);
+        // handle duplicates
+        while (l < r && nums[l] == nums[l + 1]) l++;
+        while (l < r && nums[r] == nums[r - 1]) r--;
 
-            if (nums[mid] <= nums[n - 1]) {
-                pt = mid;
-                r = mid - 1; // pivot can be on left side
-            }
-            else {
-                l = mid + 1; // pivot lies on right side
-            }
+        int mid = l + (r - l) / 2;
+
+        // right half is sorted, pivot may be mid or left side
+        if (nums[mid] <= nums[r]) {
+            pt = mid;
+            r = mid;
         }
-
-        return pt;
+        else {
+            // pivot lies on right side
+            l = mid + 1;
+        }
     }
+
+    return l; // or return pt;
+}
     
     bool binarySearch(vector<int>& nums, int l, int r, int& t) {
         while(l <= r) {
