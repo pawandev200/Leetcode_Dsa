8f73738aaf8f8e126843a45629e66d344eaf119c
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2026-05-17 10:02:02 +0530

    Time: 0 ms (100%), Space: 40.2 MB (24.36%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public: 
-    vector<bool>vis; 
     vector<int>dp; 
     bool solved(int i, vector<int>&nums){
         if(i<0 || i>= nums.size()) return false; 
         if(nums[i] == 0) return true; 
-        if(vis[i]) return false; 
 
         if(dp[i]!=-1) return dp[i];
         
-        vis[i] = true;
+        dp[i] = false; // temp value just to prevent cycle, overided 
         int f = solved(i+nums[i], nums);
         int b = solved(i-nums[i], nums);
 
@@ -17,8 +15,8 @@ public:
     }
     bool canReach(vector<int>& arr, int st) {
         int n = arr.size();
-        vis.assign(n, false);
         dp.assign(n, -1);
+
         return solved(st, arr);
     }
 };
